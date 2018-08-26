b852503a41456a811ed5ce7a81be68ac309c255f
walchko <walchko@users.noreply.github.com>
2018-08-26 15:10:08 +0000

    added contrib module

--- opencv3/build-opencv.sh
+++ opencv3/build-opencv.sh
@@ -34,13 +34,15 @@ fi
 
 echo "start ---------------"
 
-rm -fr opencv-dpkg
-
-python3folder=python3.5
+# rm -fr opencv-dpkg
 
 OPENCV_VERSION=$1
 CURRDIR=$(pwd)
-OPENCV_INSTALL_DIR="$CURRDIR/opencv-dpkg/usr/local"
+# OPENCV_INSTALL_DIR="$CURRDIR/opencv-dpkg/usr/local"
+OPENCV_INSTALL_DIR="$CURRDIR/opencv-dpkg/home/pi/.local"
+
+# python is installed here
+export  PATH=/home/pi/.local/bin:$PATH
 
 echo ""
 echo "-------------------------------------------------"
@@ -59,33 +61,36 @@ echo "----------------------------"
 echo "Updating things python libs"
 echo "----------------------------"
 
-pip install -U pip setuptools wheel
-pip install -U numpy PyYAML matplotlib simplejson
+#pip install -U pip setuptools wheel
+#pip install -U numpy PyYAML matplotlib simplejson
 
 pip3 install -U pip setuptools wheel
 pip3 install -U numpy PyYAML matplotlib simplejson
 
 # fix permissions from above operations
-chown -R pi:pi /usr/local
+chown -R pi:pi /home/pi/.local
 
 # python 2
-PY2LIB=$(python -c "from distutils.sysconfig import get_config_var;from os.path import dirname,join ; print(join(dirname(get_config_var('LIBPC')),get_config_var('LDLIBRARY')))")
-PY2INCLUDE=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")
-PY2EXE=$(which python)
-PY2NUMPY=$(python -c "import numpy; print(numpy.get_include())")
+#PY2LIB=$(python -c "from distutils.sysconfig import get_config_var;from os.path import dirname,join ; print(join(dirname(get_config_var('LIBPC')),get_config_var('LDLIBRARY')))")
+#PY2INCLUDE=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")
+#PY2EXE=$(which python)
+#PY2NUMPY=$(python -c "import numpy; print(numpy.get_include())")
 #PY2PKGS=$(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")
-PY2PKGS=$OPENCV_INSTALL_DIR/lib/python2.7/dist-packages
+#PY2PKGS=$OPENCV_INSTALL_DIR/lib/python2.7/dist-packages
 
 # python 3
-PY3LIB=$(python3 -c "from distutils.sysconfig import get_config_var;from os.path import dirname,join ; print(join(dirname(get_config_var('LIBPC')),get_config_var('LDLIBRARY')))")
+# PY3LIB=$(python3 -c "from distutils.sysconfig import get_config_var;from os.path import dirname,join ; print(join(dirname(get_config_var('LIBPC')),get_config_var('LDLIBRARY')))")
 PY3INCLUDE=$(python3 -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")
 PY3EXE=$(which python3)
 PY3NUMPY=$(python3 -c "import numpy; print(numpy.get_include())")
-#PY3PKGS=$(python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")
-PY3PKGS=$OPENCV_INSTALL_DIR/lib/${python3folder}/dist-packages
+PY3PKGS=$(python3 -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")
+#PY3PKGS=$OPENCV_INSTALL_DIR/lib/${python3folder}/dist-packages
 
-if [ ! -f $OPENCV_VERSION.tar.gz ]; then
-  wget https://github.com/opencv/opencv/archive/$OPENCV_VERSION.tar.gz
+PY3LIB="/home/pi/.local/lib/libpython3.7m.a"
+
+if [ ! -f opencv-$OPENCV_VERSION.tar.gz ]; then
+  wget -O opencv-$OPENCV_VERSION.tar.gz https://github.com/opencv/opencv/archive/$OPENCV_VERSION.tar.gz
+  wget -O opencv_contrib-$OPENCV_VERSION.tar.gz https://github.com/opencv/opencv_contrib/archive/$OPENCV_VERSION.tar.gz
 else
   echo ""
   echo "<<< Using previously downloaded file >>>"
@@ -95,30 +100,44 @@ fi
 # clean out the old
 if [ -d opencv-$OPENCV_VERSION ]; then
   echo "*** Deleting opencv-${OPENCV_VERSION} ***"
-  sleep 5
+  sleep 1
   rm -fr opencv-$OPENCV_VERSION
+  rm -fr opencv_contrib-$OPENCV_VERSION
 fi
 
 # setup things
-tar -xzf $OPENCV_VERSION.tar.gz
+tar -xzf opencv_contrib-$OPENCV_VERSION.tar.gz
+tar -xzf opencv-$OPENCV_VERSION.tar.gz
 mkdir opencv-$OPENCV_VERSION/build
 cd opencv-$OPENCV_VERSION/build
 
+#-DPYTHON2_PACKAGES_PATH=$PY2PKGS \
+#-DPYTHON2_LIBRARY=$PY2LIB \
+#-DPYTHON2_INCLUDE_DIR=$PY2INCLUDE \
+#-DPYTHON2_NUMPY_INCLUDE_DIRS=$PY2NUMPY \
+#-DPYTHON2_EXECUTABLE=$PY2EXE \
+
+# https://github.com/opencv/opencv/blob/master/CMakeLists.txt
+# I have disabled the gui with qt/gtk=off
 cmake -DCMAKE_BUILD_TYPE=RELEASE \
 -DCMAKE_INSTALL_PREFIX=$OPENCV_INSTALL_DIR \
+-D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-$OPENCV_VERSION/modules \
+-D OPENCV_ENABLE_NONFREE=ON \
 -D BUILD_WITH_DEBUG_INFO=OFF \
 -D BUILD_DOCS=OFF \
 -D BUILD_EXAMPLES=OFF \
 -D BUILD_TESTS=OFF \
 -D BUILD_opencv_ts=OFF \
 -D ENABLE_NEON=ON \
+-D ENABLE_VFPV3=ON \
+-D WITH_EIGEN=ON \
+-D WITH_TBB=ON \
+-D WITH_V4L=ON \
 -D WITH_LIBV4L=ON \
--DBUILD_opencv_python2=ON \
--DPYTHON2_PACKAGES_PATH=$PY2PKGS \
--DPYTHON2_LIBRARY=$PY2LIB \
--DPYTHON2_INCLUDE_DIR=$PY2INCLUDE \
--DPYTHON2_NUMPY_INCLUDE_DIRS=$PY2NUMPY \
--DPYTHON2_EXECUTABLE=$PY2EXE \
+-D WITH_FFMPEG=ON \
+-D WITH_QT=OFF \
+-D WITH_GTK=OFF \
+-DBUILD_opencv_python2=OFF \
 -DBUILD_opencv_python3=ON \
 -DPYTHON3_PACKAGES_PATH=$PY3PKGS \
 -DPYTHON3_LIBRARY=$PY3LIB \
--- opencv3/update-opencv.sh
+++ opencv3/update-opencv.sh
@@ -14,16 +14,18 @@ echo ""
 
 apt-get update
 apt-get -y upgrade
-apt-get -y install build-essential cmake git pkg-config python-dev swig
+apt-get -y install build-essential cmake git pkg-config swig
 apt-get -y install libeigen3-dev
+apt-get -y install libtbb2 libtbb-dev
 apt-get -y install ffmpeg
-apt-get -y install libjpeg-dev libtiff5-dev libjasper-dev libpng-dev libpng-tools
+apt-get -y install libjpeg-dev libtiff-dev libjasper-dev libpng-dev libpng-tools
 apt-get -y install libv4l-dev v4l-utils
-apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+apt-get -y install libavcodec-dev libavformat-dev libswscale-dev
 apt-get -y --force-yes install libxvidcore-dev libx264-dev
 apt-get -y install libatlas-base-dev gfortran
-apt-get -y install python2.7-dev python3-dev
-apt-get -y install libgtk2.0-dev
+# apt-get -y install python2.7-dev
+# apt-get -y install python3-dev
+# apt-get -y install libgtk2.0-dev
 apt-get autoremove -y
 
 # old
@@ -44,9 +46,9 @@ echo "============================="
 echo ""
 
 chown -R pi:pi /home/pi
-chown -R pi:pi /usr/local
-chown -R pi:pi /usr/lib/python2.7/dist-packages/
-chown -R pi:pi /usr/lib/python3.*/dist-packages/
+#chown -R pi:pi /usr/local
+#chown -R pi:pi /usr/lib/python2.7/dist-packages/
+#chown -R pi:pi /usr/lib/python3.*/dist-packages/
 
 echo ""
 echo "============================="
