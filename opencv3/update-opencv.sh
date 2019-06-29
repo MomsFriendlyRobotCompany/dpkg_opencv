8eb85f5e8f3d7914d6f924e75bc82152d0df4a37
Kevin <walchko@users.noreply.github.com>
2019-06-29 16:04:14 -0400

    Update update-opencv.sh

--- opencv3/update-opencv.sh
+++ opencv3/update-opencv.sh
@@ -29,7 +29,7 @@ apt -y install libavresample-dev
 apt-get -y install libavcodec-dev libavformat-dev libswscale-dev
 # apt-get -y --force-yes install libxvidcore-dev libx264-dev
 apt-get -y install libatlas-base-dev gfortran
-apt -y install libprotobuf-dev protobuf-compiler
+# apt -y install libprotobuf-dev protobuf-compiler
 # apt-get -y install python2.7-dev
 # apt-get -y install python3-dev
 # apt-get -y install libgtk2.0-dev
